b97acbf9d005b3ee1b5af4e554fc3ea4347eef0c
aagualb451 <aagualb451@g.educaand.es>
2023-10-13 21:52:19 +0200

    perfecto

---
+++ MyApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyLibrary;
+
+namespace MyApplication
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MyClass test = new MyClass();
+
+            Console.WriteLine("Hello {0}", test);
+        }
+    }
+}
